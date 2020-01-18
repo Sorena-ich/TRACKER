5176097bad494b2a3e4a0159af478a6ffcdb305a
Sorena-ich <59765235+Sorena-ich@users.noreply.github.com>
2020-01-18 03:40:01 +0330

    Delete post.php

--- post.php
+++
@@ -1,9 +0,0 @@
-//code by Aryan Chehreghani
-//TRACKER
-//t.me/NullTM
-<?php
-$json = (file_get_contents('php://input'));
-$dejson = json_decode($json, true);
-$location = $dejson['data'];
-shell_exec("python3 send.py ".$location);
-?>
